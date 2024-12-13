b6e9b379602c1b47ca7e9c1e660fdcf82eac857d
Анастасия Метелкина <nm.met@mail.ru>
2024-12-13 13:18:06 +0700

    Добавление класса Segment

--- src/Segment.java
+++ src/Segment.java
@@ -1,2 +1,27 @@
+
 public class Segment {
+    private int x, y;
+
+    public Segment(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
